fc22c74cd5e923ffae46f78ff06d0638570047ef
yang.wang <yang.wang@xtransfer.cn>
2022-07-07 23:49:26 +0800

    feat: 声明文件打包，压缩打包

--- rollup.config.ts
+++ rollup.config.ts
@@ -6,39 +6,50 @@ import resolve from "@rollup/plugin-node-resolve";
 // 转化 commonjs 为 esm
 import commonjs from "@rollup/plugin-commonjs";
 // 压缩代码
-// import { terser } from "rollup-plugin-terser";
-//
+import { terser } from "rollup-plugin-terser";
+// 编译css预处理器
 import postcss from "rollup-plugin-postcss";
+import dts from "rollup-plugin-dts";
 import packageJson from "./package.json";
 
+const plugins = [
+  typescript({ tsconfig: "./tsconfig.json", exclude: "./example" }),
+  resolve(),
+  commonjs(),
+  postcss(),
+];
+
+
 const config: RollupOptions[] = [
+  // index打包
   {
     input: "src/index.ts",
-    plugins: [
-      typescript({ tsconfig: "./tsconfig.json" }),
-      resolve(),
-      commonjs(),
-      // terser(),
-      postcss(),
-    ],
+    external: ["react"],
+    plugins,
     output: [
       {
-        // file: packageJson.main,
-        dir: "dist/cjs",
-        format: "cjs",
-        sourcemap: true,
-        name: "react-preview",
-      },
-      {
         file: packageJson.module,
-        // dir: "dist/esm",
         format: "esm",
         sourcemap: true,
       },
+      // 压缩文件打包
+      {
+        file: "dist/index.min.js",
+        format: "esm",
+        sourcemap: true,
+        plugins: [terser()],
+      },
     ],
   },
-  // {
-  //   input: "src/index.ts",
-  // },
+  // 声明文件打包
+  {
+    input: "src/index.ts",
+    // external: ["react-preview"],
+    output: {
+      file: packageJson.types,
+      format: "esm",
+    },
+    plugins: [dts()],
+  },
 ];
 export default config;
---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+import { Preview } from "./Preview";
+import { Button } from "./Button";
+
+export { Preview, Button };
